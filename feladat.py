8bf6f786c6dfce1a5b44b3dd8a4a282a7c726684
OsziMisi <oroszlanmihaly@turr.hu>
2022-03-21 13:25:26 +0100

    321

--- feladat.py
+++ feladat.py
@@ -6,10 +6,29 @@ ablak1.title("IKT Tkinter")
 icon = PhotoImage(file = gyoker + "cica.gif")
 ablak1.iconphoto(True, icon)
 ablak1.config(background = "white")
+elsokep = PhotoImage(file = gyoker + "cica.gif", width = 500, height = 500)
+
+cimke = Label (ablak1, 
+               text='Szia Uram! Kutyádat sétáltatod?!',
+               fg='black',
+                bg = "#c3cee0").pack()
+
+name_var=tk.StringVar()
+passw_var=tk.StringVar()
+ 
+def submit():
+     
+    name=name_var.get()
+    password=passw_var.get()
+     
+    print("The name is : " + name)
+    print("The password is : " + password)
+     
+    name_var.set("")
+    passw_var.set("")
 
 
-cimke = Label (foablak, text='Szia Uram! Kutyádat sétáltatod?!', fg='black')
-cimke.pack(side = TOP)
+
 
 ablak1.mainloop()           
 
