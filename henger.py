a6a36a4e839d8b7a3280ac8bedda54a4ad4d4dc2
OsziMisi <oroszlanmihaly@turr.hu>
2022-03-22 11:35:12 +0100

    322

---
+++ henger.py
@@ -0,0 +1,42 @@
+'''
+Feladat:
+Henger sugara es magassaag CM-ben.
+Sulya
+terfogata
+*Kerekitsuk ket tizedere*
+'''
+
+from tkinter import * 
+
+def osszeg():
+    a = int(mezo1.get())
+
+    b = int (mezo2.get())
+
+    c=a+b
+
+    mezo3.delete(0, END)
+    mezo3.insert(0,'Összeg ' +str(c))
+ablaksok = Tk()
+ablaksok.title('IKT henger kiszamito program nagyon menooo xd')
+ablaksok.config(background = "white")
+
+cimke = Label (ablaksok, text='Szia Uram! Kutyádat sétáltatod?!', fg='black')
+cimke.pack(side = TOP)
+gomb4 = Button(ablaksok, text = 'Majd', command = osszeg)
+gomb4.pack(side = RIGHT)
+gomb1 = Button(ablaksok, text = 'OK')
+gomb1.pack(side = RIGHT) 
+gomb2 = Button(ablaksok, text = 'Mégse')
+gomb2.pack(side = RIGHT)
+gomb3 = Button(ablaksok, text='Bezar', command=ablaksok.destroy)
+gomb3.pack(side = RIGHT)
+mezo1 = Entry(ablaksok)
+mezo1.pack(side = RIGHT)
+mezo2 = Entry(ablaksok)
+mezo2.pack(side = RIGHT)
+
+
+
+ablaksok.mainloop()
+
