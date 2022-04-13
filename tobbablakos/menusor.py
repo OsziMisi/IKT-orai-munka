0bb618a783f921bfa22dc38dc5158635f1e57da6
OsziMisi <78954799+OsziMisi@users.noreply.github.com>
2022-04-13 11:33:57 +0200

    413
    Foablak hozzaadasa, nevjek, kilepes gomb, menugombok

--- tobbablakos/menusor.py
+++ tobbablakos/menusor.py
@@ -14,36 +14,40 @@ def nevjegy():
 #Felszin 
 def felszin():
     def szamit():
-    a = eval(mezol get())
-    b=eval(mezo2.get())
-    c= eval(mezo3.get())
-    felszin = 2*(a*b+a*c+bc)
-    mezo4.delete(0,END)
-    mezo4.insert(0,str(felszin))
+        a = eval(mezo1.get())
+        b = eval(mezo2.get())
+        c = eval(mezo3.get())
+        felszin = 2*(a*b+a*c+bc)
+        mezo4.delete(0,END)
+        mezo4.insert(0,str(felszin))
     
 abl3 = Toplevel(foablak)
 abl3.title('A téglatest felszíne') 
 abl3.minsize(width=600, height=200)
-szoveg1= Label(ab13, text='a:')
-szoveg2=Label(ab13, text='b:')
-szoveg3=Label(ab13, text='c:") szoveg4 Label(abl3, text='Eredmény:')
-gombl-Button(ab13, text='Számítás', command=szamit) mezol- Entry(ab13)
-mezo2= Entry(ab13) mezo3 = Entry(ab13)
+szoveg1 = Label(ab13, text='a:')
+szoveg2 = Label(ab13, text='b:')
+szoveg3 = Label(ab13, text='c:') 
+szoveg4 = Label(abl3, text='Eredmény:')
+gombl = Button(ab13, text='Számítás', command=szamit) 
+mezo1=Entry(ab13)
+mezo2 = Entry(ab13) 
+mezo3 = Entry(ab13)
 mezo4 = Entry(abl3)
-szoveg.grid row =1) szoveg2.grid(row =2)
+szoveg.grid(row =1) 
+szoveg2.grid(row =2)
 szoveg3.grid(row =3)
 szoveg4.grid(row =5)
-gombl.grid(row-4, column-2, sticky
-mezo2.grid(row-2, column=2, sticky =W)
+gombl.grid(row=4, column=2, sticky= W)
+mezo2.grid(row=2, column=2, sticky =W)
 mezol.grid(row =1, column =2, sticky =W)
 mezo3.grid(row =3, column=2, sticky =W)
-mezo4.grid(row =5, column -2, sticky =W)
+mezo4.grid(row =5, column = 2, sticky =W)
 abl3.mainloop
 #Felszín ablak vége
 
 #Térfogat ablak 
 
-def terfogat:
+def terfogat():
     def szamit():
         a = eval(mezo1.get())
         b = eval(mezo2.get())
@@ -68,4 +72,35 @@ def terfogat:
     szoveg2.grid(row = 2)
     szoveg3.grid(row = 3)
     szoveg4.grid(row = 4)
-    
+    gomb1.grid(row = 4, column = 2, sticky = W)
+    mezo1.grid(row = 1, column = 2, sticky = W)
+    mezo2.grid(row = 2, column = 2, sticky = W)
+    mezo3.grid(row = 3, column = 2, sticky = W)
+    mezo4.grid(row = 4, column = 2, sticky = W)
+    abl3.mainloop()
+#terfogat alja
+
+#foablak
+foablak = tk()
+foablak.title(' A teglatest adatai')
+foablak.minsize(width = 400, height = 200)
+
+menusor = Frame(foablak)
+menusor.pack(side = TOP, fill = X)
+
+menu1 = Menubutton(menusor, text = 'File', underline = 0)
+menu1.pack(side = LEFT)
+file = Menu(menu1)
+file.add_command(label = 'Nevjegy', command = nevjegy, underline = 0)
+file.add_command(label = 'Kilep', command = foablak.destroy, underline = 0)
+menu1.config(menu = file)
+
+menu2 = Menubutton(menusor, text = 'Teglatest', underline = 0)
+menu2.pack(side = LEFT)
+teglatest = Menu(menu2)
+teglatest.add_command(label = 'Felszin', command = felszin, underline = 0)
+teglatest.add_command(lebel = 'Terfogat', command = terfogat, underline = 0)
+menu2.config(menu = teglatest)
+
+
+foablak.mainloop()
